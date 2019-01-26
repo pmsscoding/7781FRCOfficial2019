5b4f997a104948ddc4516794c48d512c879cab93
Roy Li <roygagali@gmail.com>
2019-01-26 12:16:37 -0800

    wpi lib version updated, throttle adjust sensitivity

--- robotcontrol/src/main/java/frc/robot/Robot.java
+++ robotcontrol/src/main/java/frc/robot/Robot.java
@@ -8,11 +8,13 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.SerialPort;
 import com.kauailabs.navx.IMUProtocol.GyroUpdate;
-import com.kauailabs.navx.AHRS;
+import com.kauailabs.navx.frc.AHRS;
 import com.kauailabs.navx.frc.*;
 import com.ctre.phoenix.motorcontrol.InvertType;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj.Joystick;
 import com.ctre.phoenix.motorcontrol.can.*;
 import edu.wpi.first.wpilibj.AnalogInput;
@@ -34,9 +36,9 @@ public class Robot extends TimedRobot {
 	//GYRO and PID setting
 	double setangle = 0;
 	double integral, previous_error, rcw, derivative = 0;
-	double P = 0.05;
-	double I = 0.005;
-	double D = 0.01;
+	double P = 0.02;
+	double I = 0.05;
+	double D = 0.05;
 	AHRS ahrs;
 	
 
@@ -47,6 +49,7 @@ public class Robot extends TimedRobot {
 			ahrs = new AHRS(Port.kUSB1);
 		} catch(RuntimeException ex) {
 			System.out.println("navx gyro error");
+		}
 		/* Not used in this project */
 	}
 	
@@ -64,8 +67,8 @@ public class Robot extends TimedRobot {
 		_rightSlave1.follow(_frontRightMotor);
 		
 		/* Set Neutral mode */
-		_frontLeftMotor.setNeutralMode(NeutralMode.Coast);
-		_frontRightMotor.setNeutralMode(NeutralMode.Coast);
+		_frontLeftMotor.setNeutralMode(NeutralMode.Brake);
+		_frontRightMotor.setNeutralMode(NeutralMode.Brake);
 		
 		/* Configure output direction */
 		_frontLeftMotor.setInverted(false); // <<<<<< Adjust this until robot drives forward when stick is forward
@@ -74,6 +77,7 @@ public class Robot extends TimedRobot {
 		
 		_rightSlave1.setInverted(InvertType.FollowMaster);
 		System.out.println("drive");
+		setangle = ahrs.getYaw();
 	}
 	
 	@Override
@@ -82,26 +86,41 @@ public class Robot extends TimedRobot {
 		double forward = -1 * _gamepad.getY();
 		double turn = _gamepad.getTwist();
 		boolean trigger = _gamepad.getTrigger();
-		double sensitivity = 0.4;
+		double sensitivity =1-( _gamepad.getThrottle() + 1)/2;
+		System.out.println(sensitivity);
+
 		
 		//gyro pid processing
 		GyroPID();
+		
 		boolean angletrigger = _gamepad.getTop();
 		if (angletrigger) {
-			System.out.println("setpoint for gyro at: ", ahrs.getYaw());
-			setangle = Ahrs.getYaw();
+			System.out.println(ahrs.getYaw());
+			setangle = ahrs.getYaw();
+		}
 			
 		forward = Deadband(forward);
 		turn = Deadband(turn);
+		
 
 		if (trigger) {
-			forward *= sensitivity;
-			turn *= sensitivity;
+			if (forward>0.45|turn>0.45) {
+				while (forward>0.45) {
+					forward = forward*0.9;
+				}
+				while (turn>0.45){
+					turn = turn*0.9;
+				}
+			}
+			if (forward<0.45 & turn<0.45){
+				forward *= sensitivity;
+				turn *= sensitivity;
+			}
 		}
-
-		/* Arcade Drive using PercentOutput along with Arbitrary Feed Forward supplied by turn */
-		_drive.arcadeDrive(turn+rcw, forward);
-	}
+		
+		_drive.arcadeDrive(turn, forward);
+	    }
+	
 
 	/** Deadband 5 percent, used on the gamepad */
 	double Deadband(double value) {
@@ -117,9 +136,18 @@ public class Robot extends TimedRobot {
 		return 0;
 	}
 	public void GyroPID(){
+
 		double difference = setangle - ahrs.getYaw();
-		this.previous_error = differenece;
-		this.integral +=(difference*0.02);
+		
+		this.previous_error = difference;
+		if (difference < 60 && difference > -60){
+			this.integral +=(difference*0.02);
+
+		}else{
+			this.integral = 0;
+
+		}
 		derivative = (difference - this.previous_error)/0.02;
-		this.rcw = P*difference + I*this.integral + D*derivative;
+		this.rcw = P*difference + I*this.integral - D*derivative;
+	}
 }
